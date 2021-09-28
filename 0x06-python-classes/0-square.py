6e02bdd241dd78757ba28145018d26c76c1f222a
angelicarm3 <angie00069@gmail.com>
2021-09-28 18:30:53 -0500

    Documentation espaces

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Create class Square"""
+
+
 class Square:
     """Definition class square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Create class Square with size"""
+
+
 class Square:
     """Define class Square"""
     def __init__(self, new_size):
