bada1e4724564b9b6158dc9daf921498e634951c
angelicarm3 <angie00069@gmail.com>
2021-10-12 21:09:32 -0500

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Function that writes a string to a text file (UTF8) and returns"""
+""" the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
