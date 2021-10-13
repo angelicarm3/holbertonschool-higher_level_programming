6d5ecd8fbf3b9f10fd17d1c55a6eb9a672e5c746
angelicarm3 <angie00069@gmail.com>
2021-10-12 21:48:56 -0500

    pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Function that adds all arguments to a Python list, and then save them to a file"""
+"""Function that adds all arguments to a Python list and save them to a file"""
 import sys
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
