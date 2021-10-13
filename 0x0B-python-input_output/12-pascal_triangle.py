81602b52e225e54f1f3eb65397ad8960ab30341e
angelicarm3 <angie00069@gmail.com>
2021-10-12 22:33:37 -0500

    Tsk 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Function that returns a list of lists of integers"""
+
+
+def pascal_triangle(n):
+    """Representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+
+    base = [[1]]
+    while len(base) != n:
+        tri = base[-1]
+        temp = [1]
+        for i in range(len(tri)) - 1):
+            temp.append(tri[i] + tri[i + 1])
+        temp.append(1)
+        base.append(temp)
+    return base
