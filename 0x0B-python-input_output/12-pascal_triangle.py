49d725707d9928369ae778d9746b6af67ac9bc4e
angelicarm3 <angie00069@gmail.com>
2021-10-12 22:35:57 -0500

    pep8

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
     while len(base) != n:
         tri = base[-1]
         temp = [1]
-        for i in range(len(tri)) - 1):
+        for i in range(len(tri) - 1):
             temp.append(tri[i] + tri[i + 1])
         temp.append(1)
         base.append(temp)
