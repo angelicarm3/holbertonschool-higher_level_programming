6b9f2ea639e8faf4b7a9e7020a9c5988e4f394c3
angelicarm3 <angie00069@gmail.com>
2021-10-12 22:04:43 -0500

    pep8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Function that returns the dictionary description with simple data structure"""
+"""Function that returns the dictionary description"""
+"""with simple data structure"""
 
 
 def class_to_json(obj):
