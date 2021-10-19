d74f047a53bd2f88808fbb1fe6a681d073ce6475
angelicarm3 <angie00069@gmail.com>
2021-10-18 22:26:26 -0500

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,66 @@
 #!/usr/bin/python3
+
 """Class Square"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Defines a Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for Square class"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Prints Square's info"""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.width)
+
+    def update(self, *args, **kwargs):
+        """Updates class info"""
+        if args and len(args) != 0:
+            ar = 0
+            for arg in args:
+                if ar == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif ar == 1:
+                    self.size = arg
+                elif ar == 2:
+                    self.x = arg
+                elif ar == 3:
+                    self.y = arg
+                ar += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "id":
+                    if val is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = val
+                elif key == "size":
+                    self.size = val
+                elif key == "x":
+                    self.x = val
+                elif key == "y":
+                    self.y = val
+
+    def to_dictionary(self):
+        """Returns dictionary representation of Square"""
+        return {"id": self.id, "size": self.size,
+                "x": self.x, "y": self.y}
