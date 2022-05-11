9ef921cf49256a5a593847bff7abc733893f9c4d
angelicarm3 <angie00069@gmail.com>
2022-05-10 19:19:43 -0500

    4-5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in cursor.fetchall() if ct[4] == sys.argv[4]]))
