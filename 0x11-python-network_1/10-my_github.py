bea85a3fa8caad6a613d21a6d9dfb8f10b5af00f
angelicarm3 <angie00069@gmail.com>
2022-05-19 21:18:18 -0500

    8-9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""akes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    resp = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    print(resp.json().get("id"))
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""akes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) == 1:
+        value = ""
+    else:
+        value = sus.argv[1]
+
+    resp = requests.post('http://0.0.0.0:5000/search_user', data={'q': value})
+    try:
+        response = resp.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
